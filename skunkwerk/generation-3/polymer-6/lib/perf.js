d738d3a389c7bd0ff1b39591abee1bb78f8ec7f9
Scott J. Miles <sjmiles@google.com>
2014-09-11 17:15:56 -0700

    add simple complexity measurement

--- skunkwerk/generation-3/polymer-6/lib/perf.js
+++ skunkwerk/generation-3/polymer-6/lib/perf.js
@@ -10,7 +10,26 @@ console.perfEnd = function() {
       time = (Date.now() - time) + 'ms';
       console.profileEnd();
       document.title = time;
-      console.log('From start to after first paint (?): ' + time);
+      console.log('From body start to after first paint (?): ' + time);
+      console.log('Complexity: ' + complexity(document.body));
     });
   });
+}
+
+function complexity(root) {
+  var rating = 0;
+  // polymer element?
+  if (root.created) {
+    rating += 2;
+  }
+  // has shadowRoot?
+  if (root.shadowRoot) {
+    rating += 1 + complexity(root.shadowRoot);
+  }
+  // has children?
+  rating += root.childNodes.length;
+  for (var c=root.firstElementChild; c; c=c.nextElementSibling) {
+    rating += complexity(c);
+  }
+  return rating;
 }
